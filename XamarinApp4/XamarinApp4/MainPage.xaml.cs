aed98e9c624be191327ddfb19b3f43efa72faa3f
Owens, Jason <jowens@cardinalsolutions.com>
2019-08-06 13:42:18 -0400

    Test Project for Telerik to review

---
+++ XamarinApp4/XamarinApp4/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinApp4
+{
+    public partial class MainPage : ContentPage
+    {
+		MainPageViewModel mvm;
+        public MainPage()
+        {
+			mvm = new MainPageViewModel();
+			BindingContext = mvm;
+
+            InitializeComponent();
+        }
+    }
+}
---
+++ XamarinApp4/XamarinApp4/MainPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace XamarinApp4
+{
+	public class MainPageViewModel
+	{
+
+		private string _firstName;
+		public string FirstName {
+			get => _firstName;
+			set {
+				_firstName = value;
+			}
+		}
+		public MainPageViewModel()
+		{
+
+			FirstName = "Jason";
+		}
+	}
+}
